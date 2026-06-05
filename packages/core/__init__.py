3948757b1c24b7af84cedfba754d6fd9b052bda4
platonzhuman <platonzhuman@bk.ru>
2026-06-05 18:50:11 +0800

    added convertation function and validation and public init export all classes

--- packages/core/__init__.py
+++
@@ -1,6 +0,0 @@
-from src.config import Settings, load_settings
-from src.state import StateManager
-from src.processors.images import ImageProcessor
-from src.processors.video import VideoProcessor
-
-__all__ = ["Settings", "load_settings", "StateManager", "ImageProcessor", "VideoProcessor"]
